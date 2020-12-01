b17112be1dc46f09961d4be9fe4e7b1d2cb7fd46
Ignatius Timothy Manullang <13517044@std.stei.itb.ac.id>
2020-12-01 18:01:38 +0700

    change heroku config

--- sample_app.py
+++ sample_app.py
@@ -31,8 +31,8 @@ def error404(error):
 
 
 if os.environ.get('APP_LOCATION') == 'heroku':
-    #run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    run(host="0.0.0.0", port=5000)
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    #run(host="0.0.0.0", port=5000)
 else:
     run(host='localhost', port=8080, debug=True)
 
